ea383fb3663170d30432c2896b2a44ef76933774
Sricharan-V <vanguris04@gmail.com>
2025-10-01 19:38:42 +0530

    Added registration form UI with Bootstrap and client-side jQuery validation

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
